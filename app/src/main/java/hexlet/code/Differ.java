c77b183b02a750a55d0449c42dfc4e0b86565aca
maksim butakov <qulifis@gmail.com>
2023-03-31 20:27:02 +0000

    Improve code in Differ - stage 8

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,64 +1,63 @@
 package hexlet.code;
 
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.*;
 
 public class Differ {
     public static String generate(String filepath1, String filepath2) {
         Map<String, Object> map1 = Parser.convertToMap(filepath1);
         Map<String, Object> map2 = Parser.convertToMap(filepath2);
+        Map<String, List<Object>> diffMap = generateDiff(map1, map2);
+        String diffStr = formatDiff(diffMap);
 
-        return compareMaps(map1, map2);
+        return diffStr;
     }
 
-    private static String compareMaps(Map<String, Object> map1, Map<String, Object> map2) {
-        String difference = "{";
-        List<String> diffList = new ArrayList<>();
+    private static Map<String, List<Object>> generateDiff(Map<String, Object> map1, Map<String, Object> map2) {
+        Set<String> allKeys = new HashSet<>();
+        allKeys.addAll(map1.keySet());
+        allKeys.addAll(map2.keySet());
 
-        // Find deleted nodes - present in map1, not present in map2
-        List<String> list1 = map1.entrySet().stream()
-                .filter(e -> !map2.containsKey(e.getKey()))
-                .map(e -> String.format("\n - %s: %s", e.getKey(), e.getValue()))
-                .collect(Collectors.toList());
+        Map<String, List<Object>> diffMap = new TreeMap<>();
+        for (String key : allKeys) {
+            diffMap.put(key, new ArrayList<>(2));
+        }
 
-        // Find added nodes - not present in map1, present in map2
-        map2.entrySet().stream()
-                .filter(e -> !map1.containsKey(e.getKey()))
-                .map(e -> String.format("\n + %s: %s", e.getKey(), e.getValue()))
-                .forEach(list1::add);
+        for (Map.Entry<String, List<Object>> entry : diffMap.entrySet()) {
+            if (map1.containsKey(entry.getKey())) {
+                Object o = map1.get(entry.getKey()) == null ? "null" : map1.get(entry.getKey());
+                entry.getValue().add(o);
+            } else {
+                entry.getValue().add(null);
+            }
 
-        // Find differences in nodes, if any - present in both map1 and map2
-        map1.entrySet().stream()
-                .filter(e -> map2.containsKey(e.getKey()))
-                .flatMap(e -> {
-                    // If values in both entries equal
-                    if (e.getValue().equals(map2.get(e.getKey()))) {
-                        return Stream.of(String.format("\n   %s: %s", e.getKey(), e.getValue()));
-                    } else {
-                        // If values in both entries differ
-                        return Stream.of(
-                                String.format("\n - %s: %s", e.getKey(), e.getValue()),
-                                String.format("\n + %s: %s", e.getKey(), map2.get(e.getKey())));
-                    }
-                })
-                .forEach(list1::add);
+            if (map2.containsKey(entry.getKey())) {
+                Object o = map2.get(entry.getKey()) == null ? "null" : map2.get(entry.getKey());
+                entry.getValue().add(o);
+            } else {
+                entry.getValue().add(null);
+            }
+        }
 
-        // Convert the list of differences into a string, with the natural order preserved.
-        // Changes in map1 shown first, if the value has been changed
-        String listAsStr = list1.stream()
-                .sorted((e1, e2) -> {
-                    if (e1.substring(4, e1.indexOf(":")).equals(e2.substring(4, e2.indexOf(":")))) {
-                        // Get reverse order: "-" before "+"
-                        return -(e1.substring(2, 3).compareTo(e2.substring(2, 3)));
-                    }
-                    return e1.substring(4).compareTo(e2.substring(4));
-                })
-                .collect(Collectors.joining());
+        return diffMap;
+    }
 
-        difference = difference.concat(listAsStr);
-        return difference.concat("\n}");
+    private static String formatDiff(Map<String, List<Object>> diffMap) {
+        String diffStr = "{\n";
+
+        for (Map.Entry<String, List<Object>> entry : diffMap.entrySet()) {
+            if (entry.getValue().get(0) == null) {
+                diffStr = diffStr.concat(String.format("  + %s: %s\n", entry.getKey(), entry.getValue().get(1)));
+            } else if (entry.getValue().get(1) == null) {
+                diffStr = diffStr.concat(String.format("  - %s: %s\n", entry.getKey(), entry.getValue().get(0)));
+            } else if (entry.getValue().get(0).equals(entry.getValue().get(1))) {
+                diffStr = diffStr.concat(String.format("    %s: %s\n", entry.getKey(), entry.getValue().get(0)));
+            } else {
+                diffStr = diffStr.concat(String.format("  - %s: %s\n", entry.getKey(), entry.getValue().get(0)));
+                diffStr = diffStr.concat(String.format("  + %s: %s\n", entry.getKey(), entry.getValue().get(1)));
+            }
+        }
+
+        diffStr = diffStr.concat("}");
+        return diffStr;
     }
 }
